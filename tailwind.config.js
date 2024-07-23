2cde1717ed55fd43d2b54f7885885494b51ed4fd
Diego Borrero <die.9511@gmail.com>
2024-07-23 15:49:38 -0500

    Adding new font and setting default size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 const flowbite = require('flowbite-react/tailwind');
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['AmpleSoftPro-Regular', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [flowbite.plugin()],
+  plugins: [
+    flowbite.plugin(),
+    plugin(function ({ addBase }) {
+      addBase({ html: { fontSize: '18px' } });
+    }),
+  ],
 };
