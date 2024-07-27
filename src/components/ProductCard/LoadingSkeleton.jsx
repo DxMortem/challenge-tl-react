e52ab1f9fc49cc41acfed1619621681dfdfc7946
Diego Borrero <die.9511@gmail.com>
2024-07-26 20:01:03 -0500

    Fix loading skeleton

--- src/components/ProductCard/LoadingSkeleton.jsx
+++ src/components/ProductCard/LoadingSkeleton.jsx
@@ -4,9 +4,12 @@ const ProductCardLoadingSkeleton = () => {
   return (
     <div className="h-64 w-56 animate-pulse bg-slate-50 rounded-lg shadow-xl">
       <div className="relative w-full h-4/5 rounded-lg bg-slate-50">
-        <img src={GradientWhiteToBlack}></img>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={GradientWhiteToBlack}
+        />
       </div>
-      <div className="p-2 flex h-1/5 justify-between items-center bg-slate-50" />
+      <div className="p-2 h-1/5 justify-between items-center bg-slate-50" />
     </div>
   );
 };
