596a672a82e1b1183e928e5de7a9db19d9636809
Diego Borrero <die.9511@gmail.com>
2024-07-27 23:22:30 -0500

    Improving error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,20 +1,25 @@
 import { useRouteError } from 'react-router-dom';
+import sadDoge from '../../assets/sadDoge.jpeg';
 
 const Error = ({ definedError }) => {
   const error = useRouteError();
   console.error(error);
-
+  //
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div
+      id="error-page"
+      className="h-screen flex flex-col items-center justify-center"
+    >
+      <h1 className="text-4xl mb-4">Oops!</h1>
       <p>Sorry, an unexpected error has ocurred</p>
       <p>
-        <i>
-          {error?.status + ' ' + error?.statusText ||
+        <i className="text-red-500">
+          {error?.status + ': ' + error?.statusText ||
             error?.message ||
             definedError}
         </i>
       </p>
+      <img className="rounded-lg mt-4" src={sadDoge} />
     </div>
   );
 };
