57f5bac08f9b76c9fa01391b0bf44a01cdcef782
Diego Borrero <die.9511@gmail.com>
2024-08-01 11:41:07 -0500

    Add icons to login and logout

--- src/components/NavBar/routes.jsx
+++ src/components/NavBar/routes.jsx
@@ -1,11 +1,32 @@
+import { MdLogin, MdLogout } from 'react-icons/md';
+
 const leftMenu = [
   { path: '/home', element: 'Home', private: false },
   { path: '/products?page=1', element: 'Products', private: false },
 ];
 
 const rightMenu = [
-  { path: '/log-in', element: 'Log In', private: false, publicOnly: true },
-  { path: '/log-out', element: 'Log Out', private: true },
+  {
+    path: '/log-in',
+    element: (
+      <div className="flex gap-1 items-center">
+        <MdLogin />
+        Log In
+      </div>
+    ),
+    private: false,
+    publicOnly: true,
+  },
+  {
+    path: '/log-out',
+    element: (
+      <div className="flex gap-1 items-center">
+        <MdLogout />
+        Log Out
+      </div>
+    ),
+    private: true,
+  },
 ];
 
 export { leftMenu, rightMenu };
